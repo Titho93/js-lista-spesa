c7b84680314c1c2e3e705d7e763356039c7594a5
Titho93 <titho10.93@gmail.com>
2023-09-14 14:27:08 +0200

    [add]while in js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const listItem = ['sale', 'zucchero', 'patate', 'ragu','pasta'];
+let i = 0;
+let item;
+
+while(i < listItem.length){
+ item = listItem[i];
+ document.write(item + "<br>");
+ console.log(item)
+ i++;
+}
